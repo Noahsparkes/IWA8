0e1f1e8a18e9e0a73017b4ff33f0a9e953a49714
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-08 18:16:11 +0200

    syntax changes to the current js objects

--- file.js
+++ file.js
@@ -1,50 +1,53 @@
-const leoName = 'Leo Musvaire'
-const leoNumber = '2'
-const leoStreet = 'Church St.'
-const leoPostal = '3105'
-const leoBalance = '-10'
+const leoName = "Leo Musvaire";
+const leoNumber = "2";
+const leoStreet = "Church St.";
+const leoPostal = "3105";
+const leoBalance = "-10";
 
-const sarahName = 'Sarah    '
-const sarahSurname = 'Kleinhans'
-const sarahBalance = '-4582.21000111'
-const sarahNumber = '13'
-const sarahStreet = 'William Close'
-const sarahPostal = '0310'
+const sarahName = "Sarah    ";
+const sarahSurname = "Kleinhans";
+const sarahBalance = "-4582.21000111";
+const sarahNumber = "13";
+const sarahStreet = "William Close";
+const sarahPostal = "0310";
 
 // Only change below this line
 
 const leo = {
-	fullname:leoName,
-	balance:leoBalance,
-	accessId:'47afb389-8014-4d0b-aff3-e40203d2107f',
-	age: 24,
+  fullname: leoName,
+  balance: leoBalance,
+  accessId: "47afb389-8014-4d0b-aff3-e40203d2107f",
+  age: 24,
 
-	address = {
-		number: leoNumber,
-		street: leoStreet,
-		postalCode: leoPostal,
-	}
-}
+  leoAddress :{
+    number: leoNumber,
+    street: leoStreet,
+    postalCode: leoPostal,
+  },
+};
+
 
 const sarah = {
-	fullname: sarahName + sarahSurname,
-	age :'62',
-	accessId:'6b279ae5-5657-4240-80e9-23f6b635f7a8',
-	balance:sarahBalance,
-}
-	address = {
-		number: sarahNumber,
-		street: sarahStreet,
-		postalCode: sarahPostal,
-	}
+  fullname: sarahName + sarahSurname,
+  age: "62",
+  accessId: "6b279ae5-5657-4240-80e9-23f6b635f7a8",
+  balance: sarahBalance,
 
-console.log(leo, leo[address][postal-code])
-console.log(sarah, sarah[address][postal-code])
- 
+  sarahAddress :{
+    number: sarahNumber,
+    street: sarahStreet,
+    postalCode: sarahPostal,
+  },
+
+};
 
 
-const venter = {
+console.log(leo, leo[address][postal - code]);
+console.log(sarah, sarah[address][postal - code]);
+
+/*const venter = {
     firstname: 'Noah',
     voice: false,
     age: '22',
-}
+    console.log(venter)
+}*/
